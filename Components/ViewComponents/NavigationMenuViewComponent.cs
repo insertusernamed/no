0f71ae795e75ae7128c44bc13b5e00019525aea8
insertusernamed <113151220+insertusernamed@users.noreply.github.com>
2023-06-22 11:52:01 -0400

    local db connected and library and books views done

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using no.Models;
+
+namespace no.Components.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() {
+
+            var menuItems = new List<MenuItem>
+            {
+                new MenuItem {Controller = "Home", Action = "Index", Label = "Home"},
+                new MenuItem {Controller = "Libraries", Action = "Index", Label = "Library", DropdownItems = new List<MenuItem> {
+                    new MenuItem {Controller = "Libraries", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Libraries", Action = "Create", Label = "Create"},
+                } }, 
+                new MenuItem {Controller = "Books", Action = "Index", Label = "Library", DropdownItems = new List<MenuItem> {
+                    new MenuItem {Controller = "Books", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Books", Action = "Create", Label = "Create"},
+                } },
+                new MenuItem {Controller = "Home", Action = "Privacy", Label = "Privacy"},
+            };
+
+            return View(); 
+        }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿namespace no.Models
+{
+    public class MenuItem
+    {
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+
+        public string Label { get; set; }
+
+        public List<MenuItem> DropdownItems { get; set; }
+    }
+}
